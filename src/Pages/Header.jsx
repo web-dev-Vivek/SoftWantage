9fcde2cbef20ca1758246bff4a0dd9d3519991dc
Vivek Jha <progamervivek2020@gmail.com>
2025-07-27 20:58:47 +0530

    .

--- src/Pages/Header.jsx
+++ src/Pages/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <header className="fixed top-5 left-1/2 transform -translate-x-1/2 bg-white/10 backdrop-blur-md rounded-full w-[90vw] md:w-[50vw] h-[5vh] z-50 flex justify-center items-center shadow-lg">
+    <header className="fixed top-5 left-1/2 transform -translate-x-1/2 bg-white/10 backdrop-blur-md rounded-full w-[90vw] md:w-[50vw] h-[5vh] md:h-[10vh] z-50 flex justify-center items-center shadow-lg">
       <nav className="flex flex-wrap gap-3 p-2 md:gap-10 text-black font-semibold text-sm md:text-base">
         <Link className="" to="/">
           Home
